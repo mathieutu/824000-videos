14bdf1f139ac0fb0e3ccbdc504a3e6119bf68ee2
ADRIEN D <ad.villermois@gmail.com>
2024-07-02 07:51:21 +0200

    fix : upload files

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,33 +1,38 @@
-"use client";
-import { useState } from 'react';
+'use client';
+import { useEffect, useState } from 'react';
 import { FilePond, registerPlugin } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import { FilePondFile } from 'filepond';
 
 registerPlugin(FilePondPluginImagePreview, FilePondPluginFileValidateType);
 
 export default function Home() {
-  const [files, setFiles] = useState<File[]>([]);
+  const [apiMessage, setApiMessage] = useState<string>('');
+  const [files, setFiles] = useState<FilePondFile[]>([]);
+
+  useEffect(() => {
+    setApiMessage('');
+  }, [files]);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
     if (files.length === 0) {
-      console.error('No files selected');
+      setApiMessage('aucun fichier sélectionné');
       return;
     }
 
     const formData = new FormData();
-    files.forEach(file => formData.append('files', file));
+    files.forEach((filePondFile) => {
+      const file = filePondFile.file;
+      formData.append('file', file, file.name);
+    });
 
     try {
       const response = await fetch('/api', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
         body: formData,
       });
 
@@ -36,8 +41,7 @@ export default function Home() {
       }
 
       const uploadData = await response.json();
-      console.log(uploadData);
-
+      setApiMessage(uploadData.message);
       setFiles([]);
     } catch (error) {
       console.error(error);
@@ -45,17 +49,20 @@ export default function Home() {
   };
 
   return (
-    <div className="container">
+    <div className='container'>
       <h1>Importer vos photos ou vidéos de votre séjour :</h1>
-      <form onSubmit={handleSubmit} className="upload-form">
+      <form onSubmit={handleSubmit} className='upload-form'>
         <FilePond
-          files={files}
+          files={files as any}
           onupdatefiles={setFiles}
           allowMultiple={true}
           acceptedFileTypes={['image/*', 'video/*']}
           labelIdle='Faites glisser vos photos ou vidéos ou <span class="filepond--label-action">cliquez pour les importer</span>'
         />
-        <button type="submit" className="upload-button">Importer</button>
+        <button type='submit' className='upload-button'>
+          Importer
+        </button>
+        <p>{apiMessage}</p>
       </form>
     </div>
   );
